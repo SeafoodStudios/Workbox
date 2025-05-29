6aabeba69fecb9c703d43f7726b669cb14eef5db
SeafoodStudios <williamluqilin@gmail.com>
2025-05-29 17:05:26 -0400

    Create settings.py

---
+++ static/settings.py
@@ -0,0 +1,10 @@
+# Enter your Google email here as a Python variable:
+setting_your_email = "email@gmail.com"
+# Enter your app password here as a Python variable:
+setting_your_password = "APP_PASS"
+# Enter your Google Gemini API Key here as a Python variable:
+setting_your_gemini_api_key = "GEMINI_API_KEY"
+# Enter your students' emails as a Python list here:
+setting_student_emails = ["student1@example.com","student2@example.com"]
+# Enter your students' parents' emails as a Python list here:
+setting_parent_emails = ["parent1@example.com","parent2@example.com"]
